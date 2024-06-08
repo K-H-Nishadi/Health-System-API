01f8e8624009c8f57467b06ae6c4582ff9b208b7
Hashneo <144939730+K-H-Nishadi@users.noreply.github.com>
2024-06-08 20:26:51 +0530

    Add files via upload

---
+++ model/Patient.java
@@ -0,0 +1,34 @@
+package com.coursework.model;
+
+public class Patient extends Person {
+
+    private String medicalHistory;
+    private String currentHealthStatus;
+    
+    public Patient(){
+        super(1, "", "", "");
+        
+    }
+
+    public Patient(int id, String name, String contactInformation, String address, String medicalHistory, String currentHealthStatus) {
+        super(id, name, contactInformation, address);
+        this.medicalHistory = medicalHistory;
+        this.currentHealthStatus = currentHealthStatus;
+    }
+
+    public String getMedicalHistory() {
+        return medicalHistory;
+    }
+
+    public void setMedicalHistory(String medicalHistory) {
+        this.medicalHistory = medicalHistory;
+    }
+
+    public String getCurrentHealthStatus() {
+        return currentHealthStatus;
+    }
+
+    public void setCurrentHealthStatus(String currentHealthStatus) {
+        this.currentHealthStatus = currentHealthStatus;
+    }
+}
